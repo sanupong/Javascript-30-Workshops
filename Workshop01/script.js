14a5cb28f96aacd46881498319967caff666bae1
Sanupong Lertsatittrakul <sanupong.le@gmail.com>
2022-01-12 15:25:27 +0700

    workshop 1 complete html and css

---
+++ Workshop01/script.js
@@ -0,0 +1,14 @@
+const currency_one=document.getElementById('currency-one');
+const currency_two=document.getElementById('currency-two');
+
+const amount_one=document.getElementById('amount-one');
+const amount_two=document.getElementById('amount-two');
+
+const rateText=document.getElementById('rate');
+const swap=document.getElementById('btn');
+
+currency_one.addEventListener("change",calculateMoney);
+
+function calculateMoney(){
+    console.log("Ok");
+}
